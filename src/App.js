c05b34aa3f6b7f08d6e241e54f149849c3aadf08
YeakubSadlil <yakubsadlil@gmail.com>
2023-11-06 00:35:04 +0600

    create image gallery and add cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Box, Card, CardActionArea, CardMedia } from "@mui/material";
+import { imagedata } from "./images.js";
+import "./Gallery-Style.css";
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <Box>
+      <div className="image-grid">
+        {imagedata.map((image, index) => (
+          <div
+            key={index}
+            className={index === 0 ? "feature-image" : "grid-item"}  // Set the 1st image(index = 0) as the feature image
+          >
+            <Card>
+              <CardMedia component="img" image={image.imgPath} alt={"image"} />  
+            </Card>
+          </div>
+        ))}
+      </div>
+    </Box>
   );
 }
 
