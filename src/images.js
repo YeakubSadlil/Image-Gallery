ae214dfc816bbe21dfed92ee2df27e285b5271fd
YeakubSadlil <yakubsadlil@gmail.com>
2023-11-06 00:13:29 +0600

    load all the images

---
+++ src/images.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+import img1 from "./images/image-1.webp";
+import img2 from "./images/image-2.webp";
+import img3 from "./images/image-3.webp";
+import img4 from "./images/image-4.webp";
+import img5 from "./images/image-5.webp";
+import img6 from "./images/image-6.webp";
+import img7 from "./images/image-7.webp";
+import img8 from "./images/image-8.webp";
+import img9 from "./images/image-9.webp";
+import img10 from "./images/image-10.jpeg";
+import img11 from "./images/image-11.jpeg";
+
+export const imagedata = [
+  {
+    id: 1,
+    imgPath: img1,
+  },
+  {
+    id: 2,
+    imgPath: img2,
+  },
+  {
+    id: 3,
+    imgPath: img3,
+  },
+  {
+    id: 4,
+    imgPath: img4,
+  },
+  {
+    id: 5,
+    imgPath: img5,
+  },
+  {
+    id: 6,
+    imgPath: img6,
+  },
+  {
+    id: 7,
+    imgPath: img7,
+  },
+  {
+    id: 8,
+    imgPath: img8,
+  },
+  {
+    id: 9,
+    imgPath: img9,
+  },
+  {
+    id: 10,
+    imgPath: img10,
+  },
+  {
+    id: 11,
+    imgPath: img11,
+  }
+];
