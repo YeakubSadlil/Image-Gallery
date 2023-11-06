9aab0aaca5d0e2d5acd53f240fc1b5e43561d83f
YeakubSadlil <yakubsadlil@gmail.com>
2023-11-06 14:01:03 +0600

    remove showing the checkboxes that are unticked

--- src/App.js
+++ src/App.js
@@ -9,7 +9,12 @@ import {
   CardHeader,
   CardMedia,
   Checkbox,
+  Divider,
+  IconButton,
+  Typography,
 } from "@mui/material";
+
+import CheckCircleOutline from "@mui/icons-material/CheckCircleOutline";
 
 function App() {
   const [images, setImages] = useState(imagedata);
@@ -19,51 +24,58 @@ function App() {
   const handleMouseOver = (id) => {
     setIsHovered({ id: id, value: true });
   };
-  // console.log(isHovered)
+  
   const handleMouseOut = (id) => {
     setIsHovered({ id: id, value: false });
   };
 
-  const handleSelected = (id) => {					// store all the selected images id in an array
-    setSelectedItem((pre) => [...pre, id]);
-    // console.log(selectedItem);
+  const handleSelected = (id) => {               // handle checkbox selection
+    if (selectedItem.includes(id)) {             // if the image is already selected  and clicked to unselect remove it from the selected item list
+      const updateItem = selectedItem.filter((item) => item !== id);
+      setSelectedItem(updateItem);
+    } else {                                     // if the image is not selected and clicked to select add it to the selected item list
+      setSelectedItem((pre) => [...pre, id]);
+    }
   };
-
+  // console.log(selectedItem)
   return (
     <Box sx={{ py: "20px", px: "10px" }}>
-      <div className="image-grid">
-      {/* add image-grid class which will create grid-layout with 5 spans */}
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`grid-item ${index === 0 ? "feature-image" : ""}`}  // Set the 1st image(index = 0) as the feature image
-          >
-            <Card sx={{ border: "1px solid gray", borderRadius: "10px" }}>
-              <CardActionArea
-                onMouseOver={() => handleMouseOver(image.id)}
-                onMouseOut={() => handleMouseOut(image.id)}
-              >
-                {((isHovered.id === image.id ) ||
-                  selectedItem.includes(image.id)) && (
-                  <CardHeader
-                    sx={{ position: "absolute" }}
-                    title={
-                      <Box>
-                        <Checkbox onClick={() => handleSelected(image.id)} />
-                      </Box>
-                    }
+      <Card
+        sx={{ padding: "10px", border: "1px solid gray", borderRadius: "10px" }}
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={`grid-item ${index === 0 ? "feature-image" : ""}`}
+            >
+              <Card sx={{ border: "1px solid gray", borderRadius: "10px" }}>
+                <CardActionArea
+                  onMouseOver={() => handleMouseOver(image.id)}
+                  onMouseOut={() => handleMouseOut(image.id)}
+                >
+                  {(isHovered.id === image.id || // show checkbox for only the image that is hovered
+                    selectedItem.includes(image.id)) && ( // or show checkboxes only for the images that are selected
+                    <CardHeader // otherwise checkboxes will not be shown
+                      sx={{ position: "absolute" }}
+                      title={
+                        <Box>
+                          <Checkbox onClick={() => handleSelected(image.id)} />
+                        </Box>
+                      }
+                    />
+                  )}
+                  <CardMedia
+                    component="img"
+                    image={image.imgPath}
+                    alt={image.imgPath}
                   />
-                )}
-                <CardMedia
-                  component="img"
-                  image={image.imgPath}
-                  alt={image.imgPath}
-                />
-              </CardActionArea>
-            </Card>
-          </div>
-        ))}
-      </div>
+                </CardActionArea>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </Card>
     </Box>
   );
 }
